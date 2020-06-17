fe59f43d1191cffd6f87cead494c04a67f9a3a0c
Mark Smullen <mark@techfocus.net>
2020-06-16 22:58:41 -0500

    Add griefprevention compat

--- src/main/java/dev/dejay/lockettepro/Dependency.java
+++ src/main/java/dev/dejay/lockettepro/Dependency.java
@@ -11,12 +11,16 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.scoreboard.Team;
 
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+
 public class Dependency {
     
     protected static WorldGuardPlugin worldguard = null;
     protected static Plugin vault = null;
     protected static Permission permission = null;
     private static CoreProtectAPI coreProtectAPI;
+	protected static Plugin griefprevention = null;
 
     public Dependency(Plugin plugin) {
         // WorldGuard
@@ -32,6 +36,8 @@ public class Dependency {
             RegisteredServiceProvider<Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
             permission = rsp.getProvider();
         }
+	    // GriefPrevention
+	    griefprevention = plugin.getServer().getPluginManager().getPlugin("GriefPrevention");
 
         if (Config.coreprotect && Bukkit.getPluginManager().getPlugin("CoreProtect") != null && CoreProtect.getInstance().getAPI().APIVersion() == 6) {
             coreProtectAPI = CoreProtect.getInstance().getAPI();
@@ -48,6 +54,12 @@ public class Dependency {
                 return true;
             }
         }
+		if (griefprevention != null){
+			Claim claim = GriefPrevention.instance.dataStore.getClaimAt(block.getLocation(), false, null);
+			if (claim != null){
+				if (claim.allowBuild(player, block.getType()) != null) return true;
+			}
+		}
         return false;
     }
         
